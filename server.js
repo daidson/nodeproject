4dcb80ac1af0dfb8aedd44fc9111ed1ca0233a04
Daidson Alves <daidson.alves@gmail.com>
2020-08-07 18:36:35 -0300

    mongodb connection created, adjusting post method to save it in collection

--- server.js
+++ server.js
@@ -1,25 +1,37 @@
-console.log('Node is working just fine!')
-const express = require('express')
-const bodyParser = require('body-parser')
+console.log("Node is working just fine!");
+const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
-const MongoClient = require('mongodb').MongoClient
+const MongoClient = require("mongodb").MongoClient;
 
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.urlencoded({ extended: true }));
 
-MongoClient.connect('mongodb-connection-string', (err,client){
-    
-})
+const connectionString =
+  "mongodb+srv://luke:gi1pBfQXZ3zbXnCZ@testcluster.yquoq.gcp.mongodb.net/test?retryWrites=true&w=majority";
 
-app.listen(3000, function() {
-    console.log('Listening on port 3000')
-})
+// MongoClient.connect(connectionString, { useUnifiedTopology: true }, (err, client) => {
+//   if (err) return console.error(err);
+//   console.log("Connected to Database");
+// });
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html')
-})
+MongoClient.connect(connectionString, { useUnifiedTopology: true })
+  .then((client) => {
+    console.log("Connected to Database");
+    const db = client.db("philosophy-quotes");
+    const quotesCollection = db.collection("quotes");
+    app.listen(3000, function () {
+      console.log("Listening on port 3000");
+    });
+    app.get("/", (req, res) => {
+      res.sendFile(__dirname + "/index.html");
+    });
 
-app.post('/quotes', (req, res) => {
-    // console.log('Trying this new quote hehe.')
-    console.log(req.body)
-})
-
+    app.post("/quotes", (req, res) => {
+      quotesCollection.insertOne(req.body).then((result) => {
+        console.log(result);
+      })
+      .catch(error => console.error(error))
+      console.log(req.body);
+    });
+  })
+  .catch((error) => console.error(error));
