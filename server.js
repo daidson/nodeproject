d9a4ea722ef56eb2ce15289f7a4a0b0dc34392f1
Daidson Alves <daidson.alves@gmail.com>
2020-08-07 18:15:36 -0300

    'new dependencies added, get and put method created, starting mongodb connection'

--- server.js
+++ server.js
@@ -1,1 +1,25 @@
 console.log('Node is working just fine!')
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express();
+const MongoClient = require('mongodb').MongoClient
+
+app.use(bodyParser.urlencoded({ extended: true }))
+
+MongoClient.connect('mongodb-connection-string', (err,client){
+    
+})
+
+app.listen(3000, function() {
+    console.log('Listening on port 3000')
+})
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html')
+})
+
+app.post('/quotes', (req, res) => {
+    // console.log('Trying this new quote hehe.')
+    console.log(req.body)
+})
+
