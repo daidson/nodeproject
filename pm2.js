5ca85b6c072212317a21f252c5672e41a92b93ec
Daidson Alves <daidson.alves@gmail.com>
2020-08-09 20:33:44 -0300

    adding pm2 file

---
+++ pm2.js
@@ -0,0 +1,47 @@
+const pm2 = require("pm2");
+const instances = process.env.WEB_CONCURRENCY || -1;
+const maxMemory = process.env.WEB_MEMORY || 512;
+
+pm2.connect(function () {
+  pm2.start(
+    {
+      script: "server.js",
+      name: "wisdon-drops",
+      exec_mode: "cluster",
+      watch: false,
+      instances: instances,
+      max_memory_restar: maxMemory + "M",
+      log_date_format: "YYYY-MM-DD HH:mm Z",
+      log_file: "total.log",
+      error_file: "err.log",
+      out_file: "out.log",
+      merge_logs: true,
+      env: {
+        NODE_ENV: "production",
+      },
+    },
+    function (err) {
+      if (err) {
+        return console.error(
+          "Error while launching applications",
+          err.stack || err
+        );
+      }
+
+      console.log("PM2 and application have been successfully started");
+
+      pm2.launchBus(function (err, bus) {
+        if (err) console.log(err);
+        console.log("[PM2] Log streaming started");
+
+        bus.on("log:out", function (packet) {
+          console.log("[App:%s", packet.process.name, packet.data);
+        });
+
+        bus.on("log:err", function (packet) {
+          console.error("[App:%s][Err] %s", packet.process.name, packet.data);
+        });
+      });
+    }
+  );
+});
